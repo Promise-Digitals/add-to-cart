b7ec7b8d698ae40fec503d2651fd3f58aa312e11
Promise_digitals <promiseedeh025@gmail.com>
2024-01-27 18:06:44 -0800

    updated the files

--- myscript.js
+++ myscript.js
@@ -80,17 +80,24 @@ function addtocart(a){
     cart.push({...categories[a]});
     displaycart();
 }
+
 function delElement(a){
     cart.splice(a, 1);
     displaycart()
 }
+
 function displaycart(a){
     let j = 0;
+    let total = 0;
+    document.getElementById("count").innerHTML = cart.length;
     if(cart.length==0){
         document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "$ "+0+".00";
     }else{
         document.getElementById("cartItem").innerHTML = cart.map((items)=>{
             var {image, title, price} = items;
+            total = total + price;
+            document.getElementById("total").innerHTML = "$ "+total+".00";
             return (
                 `<div class='cart-item'>
                 <div class='row-img'>
